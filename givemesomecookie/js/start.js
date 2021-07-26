256ecec9bb91ab0f28b339368697ccfadd86c93e
ParkSaeBom <69411543+aosldk09@users.noreply.github.com>
2021-07-26 18:41:57 +0900

    Update start.js
    수정수정

--- givemesomecookie/js/start.js
+++ givemesomecookie/js/start.js
@@ -19,7 +19,7 @@ function checkOnlyOne(element) {
 
 function calResult(){
   console.log(select);
-  var result = select.indexOf(Math.max(...select));
+  const result = select.indexOf(Math.max(...select));
   return result;
 }
 
@@ -28,9 +28,9 @@ function setResult(){
   const resultName = document.querySelector('.resultname');
   resultName.innerHTML = infoList[point].name;
 
-  var resultImg = document.createElement('img');
+  const resultImg = document.createElement('img');
   const imgDiv = document.querySelector('#resultImg');
-  var imgURL = 'img/image-' + point + '.png';
+  const imgURL = 'img/image-' + point + '.png';
   resultImg.src = imgURL;
   resultImg.alt = point;
   resultImg.classList.add('img-fluid');
@@ -54,8 +54,8 @@ function goResult(){
 }
 
 function addAnswer(answerText, qIdx, idx){
-  var a = document.querySelector('.answerBox');
-  var answer = document.createElement('button');
+  const a = document.querySelector('.answerBox');
+  const answer = document.createElement('button');
   answer.classList.add('answerList');
   answer.classList.add('my-3');
   answer.classList.add('py-3');
@@ -66,14 +66,14 @@ function addAnswer(answerText, qIdx, idx){
   answer.innerHTML = answerText;
 
   answer.addEventListener("click", function(){
-    var children = document.querySelectorAll('.answerList');
+    const children = document.querySelectorAll('.answerList');
     for(let i = 0; i < children.length; i++){
       children[i].disabled = true;
       children[i].style.WebkitAnimation = "fadeOut 0.5s";
       children[i].style.animation = "fadeOut 0.5s";
     }
     setTimeout(() => {
-      var target = qnaList[qIdx].a[idx].type;
+      const target = qnaList[qIdx].a[idx].type;
       for(let i = 0; i < target.length; i++){
         select[target[i]] += 1;
       }
@@ -92,15 +92,38 @@ function goNext(qIdx){
     return;
   }
 
-  var q = document.querySelector('.qBox');
+  const q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
     addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
-  var status = document.querySelector('.statusBar');
+  const status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
+
+function begin(){
+    let value = $("input[name='size']:checked").val();
+    if (value == undefined) {
+        alert('사이즈를 선택하세요!');
+    } else {
+        main.style.WebkitAnimation = "fadeOut 1s";
+        main.style.animation = "fadeOut 1s";
+        setTimeout(() => {
+          qna.style.WebkitAnimation = "fadeIn 1s";
+          qna.style.animation = "fadeIn 1s";
+          setTimeout(() => {
+            main.style.display = "none";
+            qna.style.display = "block"
+          }, 450)
+          let qIdx = 0;
+          goNext(qIdx);
+        }, 450);
+          }
+}
+
+
+/*
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -115,3 +138,4 @@ function begin(){
     goNext(qIdx);
   }, 450);
 }
+*/
