d38ebf0ea68542419feead36b0372ef422fc02f2
Minerva08 <78466598+Minerva08@users.noreply.github.com>
2021-07-27 21:05:37 +0900

    Add files via upload
    용량별 분류..?초안

--- start.js
+++ start.js
@@ -4,149 +4,200 @@ const result = document.querySelector("#result");
 
 const endPoint = 15;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-let choice = document.choice;
-let size = choice.select.value;
-
-let single = 'single';
-let pint = 'pint';
-let quarter = 'quarter';
-let family = 'family';
-
-
+const arr =new Array(0);
 let flavor=0;
+let count=0;
 
 
-function func_validate() {
-
-    if (size === single) {
-        flavor = 1;
-
-
-    } else if (size === pint) {
-        flavor = 3;
-
-    } else if (size === quarter) {
-        flavor = 4;
-
-    } else if (size === family) {
-        flavor=5;
-         begin();
-
-    }
-    else if(size!==single && size !==pint && size !==quarter && size !== family) {
-        //체크박스가 선택되지 않을경우 실행
-        flavor =0;
-        alert("종류를 선택해 주세요");
-    }
-
-
+function checkOnlyOne(element) {
+  const checkboxes 
+      = document.getElementsByName("size");
+  
+  checkboxes.forEach((cb) => {
+    cb.checked = false;
+  })
+  
+  element.checked = true;
 }
-
-
-function begin() {
-
-    main.style.WebkitAnimation = "fadeOut 1s";
-    main.style.animation = "fadeOut 1s";
-    setTimeout(() => {
-        qna.style.WebkitAnimation = "fadeIn 1s";
-        qna.style.animation = "fadeIn 1s";
-        setTimeout(() => {
-            main.style.display = "none";
-            qna.style.display = "block"
-        }, 450)
-        let qIdx = 0;
-        goNext(qIdx);
-    }, 450);
-}
-
-
-
-
 
 function calResult() {
-    console.log(select);
-    var result = select.indexOf(Math.max(...select));
-    return result;
+  
+  let size=begin();
+ 
+  console.log(select);
+
+  const result = select.indexOf(Math.max(...select));
+  console.log(result);
+ 
+  
+  for(let i=0;i<=select.length;i++){
+
+    if(select[i]===select[result] && count<flavor-1){
+      if(i===result){
+        continue;
+      }
+      console.log(select[i]);
+      arr.push(i);
+      count++;
+    }
+    else if(select[i]===select[result]-1 && count<flavor-1){
+      if(i===result){
+        continue;
+      }
+      console.log(select[i]);
+      arr.push(i);
+      count++;
+
+    }
+    
+  }
+  console.log(arr);
+  select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  return result;
 }
 
-function setResult() {
-    let point = calResult();
-    const resultName = document.querySelector('.resultname');
-    resultName.innerHTML = infoList[point].name;
 
-    var resultImg = document.createElement('img');
-    const imgDiv = document.querySelector('#resultImg');
-    var imgURL = 'img/image-' + point + '.png';
-    resultImg.src = imgURL;
-    resultImg.alt = point;
-    resultImg.classList.add('img-fluid');
-    imgDiv.appendChild(resultImg);
 
-    const resultDesc = document.querySelector('.resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
+function setResult(){
+  let point = calResult();
+  const resultName = document.querySelector('.resultname');
+  resultName.innerHTML = infoList[point].name;
+
+  const resultImg = document.createElement('img');
+  const imgDiv = document.querySelector('#resultImg');
+  const imgURL = 'img/image-' + point + '.png';
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add('img-fluid');
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector('.resultDesc');
+  resultDesc.innerHTML = infoList[point].desc;
 }
 
-function goResult() {
-    qna.style.WebkitAnimation = "fadeOut 1s";
-    qna.style.animation = "fadeOut 1s";
+function goResult(){
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
     setTimeout(() => {
-        result.style.WebkitAnimation = "fadeIn 1s";
-        result.style.animation = "fadeIn 1s";
-        setTimeout(() => {
-            qna.style.display = "none";
-            result.style.display = "block"
-        }, 450)
-    })
+      qna.style.display = "none";
+      result.style.display = "block"
+    }, 450)})
     setResult();
 }
 
-function addAnswer(answerText, qIdx, idx) {
-    var a = document.querySelector('.answerBox');
-    var answer = document.createElement('button');
-    answer.classList.add('answerList');
-    answer.classList.add('my-3');
-    answer.classList.add('py-3');
-    answer.classList.add('mx-auto');
-    answer.classList.add('fadeIn');
+function addAnswer(answerText, qIdx, idx){
+  const a = document.querySelector('.answerBox');
+  const answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
 
-    a.appendChild(answer);
-    answer.innerHTML = answerText;
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
 
-    answer.addEventListener("click", function () {
-        var children = document.querySelectorAll('.answerList');
-        for (let i = 0; i < children.length; i++) {
-            children[i].disabled = true;
-            children[i].style.WebkitAnimation = "fadeOut 0.5s";
-            children[i].style.animation = "fadeOut 0.5s";
-        }
-        setTimeout(() => {
-            var target = qnaList[qIdx].a[idx].type;
-            for (let i = 0; i < target.length; i++) {
-                select[target[i]] += 1;
-            }
+  answer.addEventListener("click", function(){
+    const children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
+    }
+    setTimeout(() => {
+      const target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
+      }
 
-            for (let i = 0; i < children.length; i++) {
-                children[i].style.display = 'none';
-            }
-            goNext(++qIdx);
-        }, 450)
-    }, false);
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },450)
+  }, false);
 }
 
-function goNext(qIdx) {
-    if (qIdx === endPoint) {
-        goResult();
-        return;
-    }
+function goNext(qIdx){
+  if(qIdx === endPoint){
+    goResult();
+    return;
+  }
 
-    var q = document.querySelector('.qBox');
-    q.innerHTML = qnaList[qIdx].q;
-    for (let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
-    }
-    var status = document.querySelector('.statusBar');
-    status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
+  const q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+  }
+  const status = document.querySelector('.statusBar');
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
 
+
+function begin() {
+  let value = $("input[name='size']:checked").val();
+  if (value == undefined) {
+      alert('사이즈를 선택하세요!');
+  } else {
+
+      main.style.WebkitAnimation = "fadeOut 1s";
+      main.style.animation = "fadeOut 1s";
+      setTimeout(() => {
+          qna.style.WebkitAnimation = "fadeIn 1s";
+          qna.style.animation = "fadeIn 1s";
+          setTimeout(() => {
+              main.style.display = "none";
+              qna.style.display = "block"
+          }, 450)
+          let qIdx = 0;
+          goNext(qIdx);
+      }, 450);
+
+      if (value === 'single') {
+          flavor=1;
+          console.log(flavor);
+          return flavor;
+      } else if (value === 'pint') {
+          flavor = 3;
+          console.log(flavor);
+          return flavor;
+      } else if (value === 'quarter') {
+          flavor = 4;
+          console.log(flavor);
+          return flavor;
+      } else if (value === 'family') {
+          flavor = 5;
+          console.log(flavor);
+          return flavor;
+      } else if (value === 'half') {
+          flavor = 6;
+          console.log(flavor);
+          return flavor;
+      }
+
+
+  }
+
+
+}
+
+/*
+function begin(){
+  main.style.WebkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      main.style.display = "none";
+      qna.style.display = "block"
+    }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
+}
+*/
